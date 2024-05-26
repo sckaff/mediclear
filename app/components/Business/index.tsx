2a438eb8403d2ffb7327e83ed0a5e3af4ab593ca
Fernando Sckaff <sckafffernando@gmail.com>
2024-05-25 22:45:49 -0400

    Getting used to new layout folder architecture

--- app/components/Business/index.tsx
+++ app/components/Business/index.tsx
@@ -9,7 +9,7 @@ const Business = () => {
             <div className='grid grid-cols-1 lg:grid-cols-12 space-x-1'>
 
                 <div className='col-span-6 flex flex-col justify-center'>
-                    <h2 className='text-midnightblue text-4xl sm:text-5xl font-semibold text-center lg:text-start lh-143'>Make better business decisions with Paidin.</h2>
+                    <h2 className='text-midnightblue text-4xl sm:text-5xl font-semibold text-center lg:text-start lh-143'>Automate your medical coding.</h2>
                     <h3 className='text-black text-lg font-normal text-center lg:text-start lh-173 opacity-75 pt-3'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab.</h3>
                     <Link href={'/'} className="text-electricblue text-lg font-medium flex gap-2 pt-4 mx-auto lg:mx-0">
                         Learn more <Image src="/assets/people/arrow-right.svg" alt="arrow-right" width={24} height={24} />
