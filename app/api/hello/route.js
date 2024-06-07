a745d7a74355b36b423bf71a956a4510300765c2
Fernando Sckaff <sckafffernando@gmail.com>
2024-06-06 23:32:46 -0400

    almost done with setting up apis

--- app/api/hello/route.js
+++
@@ -1,6 +0,0 @@
-// /app/api/hello/route.js
-export async function GET(request) {
-  return new Response(JSON.stringify({ message: 'Hello, world!' }), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
